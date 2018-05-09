a9981621a35964420bea8af66d1d9a146d0efe18
Thulio Philipe <thulioph@gmail.com>
2018-05-09 16:18:08 -0300

    Updates the Modal stories file.

--- src/components/modal/modal.stories.js
+++ src/components/modal/modal.stories.js
@@ -5,12 +5,14 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { WithNotes } from '@storybook/addon-notes';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs, boolean, text, button } from '@storybook/addon-knobs/react';
 
 import Modal from './index';
 
 // ====
 
 const ModalObj = {
+  active: true,
   content: 'Here is my modal content!',
   handleClose: () => action('Close')(),
   handleSubmit: () => action('Submit')(),
@@ -19,31 +21,17 @@ const ModalObj = {
 
 // ====
 
-storiesOf('Modal', module)
-  .add('empty', () => (
-    <Modal />
-  ))
-  .add('with active', () => (
-    <Modal active />
-  ))
-  .add('active with handleClose', () => (
-    <Modal active handleClose={ModalObj.handleClose} />
-  ))
-  .add('active with handleSubmit', () => (
-    <Modal active handleSubmit={ModalObj.handleSubmit} />
-  ))
-  .add('active with handleCancel', () => (
-    <Modal active handleCancel={ModalObj.handleCancel} />
-  ))
-  .add('active with content', () => (
-    <Modal active content={ModalObj.content} />
-  ))
-  .add('complete version', withInfo()(() =>
-    <Modal
-      active
-      handleClose={ModalObj.handleClose}
-      handleSubmit={ModalObj.handleSubmit}
-      handleCancel={ModalObj.handleCancel}
-      content={ModalObj.content}
-    />
-  ))
+const stories = storiesOf('Modal', module);
+stories.addDecorator(withKnobs);
+
+// ====
+
+stories.add('complete version', withInfo()(() =>
+  <Modal
+    active={boolean('Active', ModalObj.active)}
+    content={text('Content', ModalObj.content)}
+    handleClose={ModalObj.handleClose}
+    handleSubmit={ModalObj.handleSubmit}
+    handleCancel={ModalObj.handleCancel}
+  />
+));
