a89b40968eaf5d8a62d2ce4644a1ea415fb0767a
Thulio Philipe <thulioph@gmail.com>
2019-01-09 20:09:13 -0300

    Updates the jest.config.js file.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
     moduleNameMapper: {
         '.*\\.(css|less|styl|scss|sass)$': '<rootDir>/tests/mocks/style.js'
     },
+    testURL: 'http://localhost',
+    setupTestFrameworkScriptFile: '<rootDir>/src/setupTests.js',
 };
