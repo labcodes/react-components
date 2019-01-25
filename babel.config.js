bda9bc2cfbf5cd4d64e077b1774808ad271617da
Thulio Philipe <thulioph@gmail.com>
2019-01-25 17:06:34 -0300

    Updates the babel.config.js file.

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "presets": [
+    "@babel/env",
+    "react",
+  ],
+
+  "plugins": [
+    "@babel/plugin-proposal-object-rest-spread",
+    "babel-plugin-styled-components"
+  ]
+};
